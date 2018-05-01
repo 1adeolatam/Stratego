6836986cc541716a31c7a335ac3ec71c4f5f2268
ooa <adeolatey@ymail.com>
2018-04-30 21:16:02 -0400

    fixi

--- Pieces/piece.cc
+++ Pieces/piece.cc
@@ -2,55 +2,52 @@
 #include "player.h"
 #include <stdlib.h>
 
-Piece :: Piece(char c): tile(NULL), color(c){
+Piece :: Piece(int colour, int type): colour(colour), type(type){
 }
 Piece :: ~Piece(){
 }
-bool Piece :: moveTo(Player& moveMaker, Tile& trgt){
 
-Piece* potCapture = NULL:
-Tile* src = tile;
-bool validMove = false;
-if(color == moveMaker.getColor()){
-	if(canMoveTo(trgt){
-		if(trgt.isOccupied()){
-			potCapture = trgt.getOccupant();
-			if(potCapture->getColor() != moveMaker.getColor()){
-					validMove = true;
-			}
-		}else{
-			validMove = true;
-		}
-		if(validMove){
-			if(potCapture){
-				potCapture->setLocation(NULL);
-			}
-			tile->setPiece(NULL);
-			tile = &trgt;
-			tile->setPiece(this);
-		}
-		}		
-	}	
-	return validMove;
-}	
-void Piece :: setLocation(Tile* trgt){
-	tile = trgt;
-}
-char Piece :: color() const{
-	return color;
+char Piece :: getColour() const{
+	return colour;
 }
 Tile* Piece :: location()const{
 	return tile;
 }
+//Almost all the stratego pieces move 1 unit in either forward
+//backward, left or right.
+//So only overriding required is for Bomb,Flag and Scout(which is only constrained by the board).
 
-bool Piece ::canMoveTo(Tile& trgt){
-	bool isVal = false;
-	int deltaFile = abs(this->location()->getFile() - trgt.getFile());
-	int deltaRank = abs(this->location()->getRank() - trgt.getRank());
-	if(deltaFile == 0 && deltaRank == 1){
-		isVal = true;
-	}else if(deltaRank == 1 && deltaFile == 0){
-		isVal = true;
+bool Piece ::move(Tile board[][],int rank, int file){
+	possibleDestinations.clear();
+	switch(type){
+	case BOMB:
+	case FLAG:
+		return possibleDestinations;
+	// No possible moves at any time for The bomb and the flag
 	}
-	return isVal;
+	int potRank[4] = {rank, rank,rank+1,rank-1};
+	int potFile[4] = {file+1,file-1,file,file};
+
+	//enumerate throught the four possible moves
+	//The board is a [10][10] Tile matrix
+	//possible ranges are [0-9][0-9]
+	//with the exclusion of the water tiles
+	//which are 2 2x2 sqaures in the middle of the field
+	
+	for(int i = 0; i < 4; ++i){
+	 if((potRank[i] >=0 && potRank < 10){
+	  if(potFile[i] >=0 && potFile < 10){
+	//Destination is on the board
+	   if(board[potRank[i]][potFile[i]] != NULL && board[potRank[i]][potFile[i]]->getPiece.getColour() != colour){
+	    //Destination has enemy piece
+	          //TODO:
+		//ADD CHECK FOR WATER TILES
+	 possibleDestinations.add(board[potRank[i]][potFile[i]]);
+	   }
+	  }
+	 }
+	}
+
+	return possibleDestinations;
 }
+
--- Pieces/piece.h
+++ Pieces/piece.h
@@ -4,7 +4,7 @@
 #include <ostream>
 #include "tile.h"
 #include <string>
-
+#include <vector>
 // The identification number of Piece types
 const int BOMB =  'B';
 const int MARSHAL = 1;
@@ -21,35 +21,32 @@ const int FLAG = 'F';
 const int WATER = -3;
 
 // Total number of each Piece type at the start of game
-const int amtBOMB =  6;
-const int amtMARSHAL = 1;
-const int amtGENERAL = 1;
-const int amtCOLONEL = 2;
-const int amtMAJOR = 3;
-const int amtCAPTAIN = 4;
-const int amtLIEUTENANT = 4;
-const int amtSERGEANT = 4;
-const int amtSAPPER = 5;
-const int amtSCOUT = 8;
-const int amtSPY = 1;
+//const int amtBOMB =  6;
+//const int amtMARSHAL = 1;
+//const int amtGENERAL = 1;
+//const int amtCOLONEL = 2;
+//const int amtMAJOR = 3;
+//const int amtCAPTAIN = 4;
+//const int amtLIEUTENANT = 4;
+//const int amtSERGEANT = 4;
+//const int amtSAPPER = 5;
+//const int amtSCOUT = 8;
+//const int amtSPY = 1;
 
 
 struct Piece{
 	private:
-	Tile * tile;
+	int type;
+	int colour;
 	protected:
-	char color;
+	vector<Tile> possibleDestiations;
 	public:
-	Piece(char c);
-	virtual ~Piece();
-	virtual bool moveTo(Player& moveMaker, Tile& trgt);
-	virtual void setLocation(Tile* trgt);
-	virtual int value() const = 0;
-	char color() const;
+	Piece(int colour, int type);
+	~Piece();
+	char getColour();
 	//display either red or blue
-	void display() const = 0;
-	bool canMoveTo(Tile& trgt) const = 0;
-	Tile* location()const;
+	virtual void display();
+	virtual vector<Tile> move(Tile[][],int rank, int file);
  };
 #endif
 
